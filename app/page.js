0ec4f1944858629d0906fbef8015ef5671d086ac
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-12 09:24:21 +0530

    nav complete

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
 import { useState } from 'react';
+import { IoIosArrowBack } from "react-icons/io";
 
 const Main = () => {
     const [inputValue, setInputValue] = useState('');
@@ -8,8 +9,17 @@ const Main = () => {
         setInputValue(event.target.value);
       };
 
-  return (
-    <div className="max-w-md mx-auto my-8 p-4 rounded-lg">
+  return (  <>
+
+  
+  <nav className="flex justify-between mb-4 border-b pb-4 pt-3">
+<div className="text-3xl font-bold"><IoIosArrowBack/></div>
+<div className="font-bold text-xl">Chat</div>
+<div></div>
+</nav>
+    <div className="max-w-md mx-auto p-4 rounded-lg">
+  
+
       <div className="mb-4">
         <div className="bg-[#e6ebeb] bg-opacity-80 w-[250px] p-2  rounded-tr-2xl rounded-bl-2xl rounded-br-2xl inline-block">
           <p className="text-[14px]">Did not help? Chat with our expert</p>
@@ -18,12 +28,12 @@ const Main = () => {
         <p className="text-[11px] text-slate-400">Few seconds ago</p>
       </div>
       <div className="mb-4 text-right">
-        <div className="bg-sky-200 p-3 rounded-tl-lg rounded-bl-lg rounded-br-lg inline-block">
+        <div className="bg-sky-200 bg-opacity-80 p-3 rounded-tl-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px] text-left"style={{ width: '250px', wordWrap: 'break-word', padding:"1px" }} >{inputValue} </p>
         </div>
       </div>
       <div className="mb-4">
-        <div className="bg-[#e6ebeb] w-[270px] p-3 pr-5 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
+        <div className="bg-[#e6ebeb] bg-opacity-80 w-[270px] p-3 pr-5 rounded-tr-lg rounded-bl-lg rounded-br-lg inline-block">
           <p className="text-[14px]">I understand how upsetting it is to loose your money.We have raised a ticket for your issue.We are working on resolving your issue and once correct and complete details are received we do action on it.</p>
         </div>
       </div>
@@ -38,6 +48,7 @@ const Main = () => {
     </div>
       
     </div>
+    </>
   );
 };
 
