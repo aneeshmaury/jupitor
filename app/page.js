db54944f7338508fc545b251ba9fd85809197d8a
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-12 09:44:55 +0530

    ewfe

--- app/page.js
+++ app/page.js
@@ -37,6 +37,7 @@ const Main = () => {
         <div className="bg-[#e6ebeb] bg-opacity-100 w-[230px] py-2 px-3 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl inline-block">
           <p className="text-[14px]">Sure, please type in your query</p>
         </div>
+           <p className="text-[11px] text-slate-400">Few seconds ago</p>
       </div>
       <div className="mb-4 text-right">
         <div className="bg-sky-200 bg-opacity-80 p-3 rounded-tl-lg rounded-bl-lg rounded-br-lg inline-block">
